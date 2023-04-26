9c171d28d93f7ae0b189e9d6bc8b146384a641cd
VivekBits2210 <viveknayak2210@gmail.com>
2023-04-25 21:26:41 -0400

    versionCode

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+module.exports = {
+    ...defaultConfig,
+    transformer: {
+      ...defaultConfig.transformer,
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+        minifierConfig: {
+          ...defaultConfig.transformer.minifierConfig,
+          sourceMap: true, // Enable source maps
+        },
+      }),
+    },
+};
